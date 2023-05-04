d38f84a3fe4d1b56e272da8f9b4ef860009b8678
Ace <shohruh.nx@outlook.de>
2023-05-04 22:37:40 +0400

    Update Calculate Execution Time.py

--- Beginner/Calculate Execution Time.py
+++ Beginner/Calculate Execution Time.py
@@ -1,6 +1,8 @@
 
 from time import time
 
+
+# ✶✶✶✶✶✶✶✶✶ CONCURRENT MEASUREMENT ✶✶✶✶✶✶✶✶✶
 start = time()
 
 # 
@@ -10,23 +12,40 @@ start = time()
 end = time()
 
 execution_time = end - start
-print(f"\nExecution Time:\n{execution_time / 60:.2f} min\n{execution_time:.2f} s\n{execution_time * 1000:.2f} ms\n")
+minutes = execution_time / 60
+milliseconds = execution_time * 1000
+
+if milliseconds >= 1000:
+    print(f"\nExecution Time:\n{minutes:.2f} min\n{execution_time:.2f} s\n")
+else:
+    print(f"\nExecution Time:\n{minutes:.2f} min\n{execution_time:.2f} s\n{milliseconds:.2f} ms\n")
 
 
 
-# try this function:
-def create_acronym(text: str) -> str:
-    words = text.split()
-    acronym = ""
-    for word in words:
-        acronym += word[0].upper()
-    return acronym
+# ✶✶✶✶✶✶✶✶✶ IMMEDIATE MEASUREMENT ✶✶✶✶✶✶✶✶✶
+def measure_exec_time(function, *args):
+    start = time()
+
+    function(*args)
+
+    end = time()
+
+    execution_time = end - start
+    minutes = execution_time / 60
+    milliseconds = execution_time * 1000
+
+    if milliseconds >= 1000:
+        print(f"\nExecution Time:\n{minutes:.2f} min\n{execution_time:.2f} s\n")
+    else:
+        print(f"\nExecution Time:\n{minutes:.2f} min\n{execution_time:.2f} s\n{milliseconds:.2f} ms\n")
 
 
-try:
-    text = input("Enter here: ")
-    acronym = create_acronym(text)
-    print(f"\n'{text}' acronym: {acronym}")
+# 
+# function here
+# 
 
-except Exception as e:
-    print(f"Problem is: {e}")
+# 
+# argument here
+# 
+
+measure_exec_time(function, argument)
