d6c1b002ed7dc7f0747a29ccaaff55855bda9ef4
Ace <shohruh.nx@outlook.de>
2023-05-04 21:18:37 +0400

    Update Internet Speedtest.py

--- Beginner/Internet Speedtest.py
+++ Beginner/Internet Speedtest.py
@@ -1,18 +1,29 @@
+
 #pip install speedtest-cli
 
 import speedtest
 
-internetSpeed = speedtest.Speedtest()
+try:
+    internetSpeed = speedtest.Speedtest()
+    download = internetSpeed.download() / 1_000_000
+    upload = internetSpeed.upload() / 1_000_000
 
-print("\nMeasuring Internet Speed\n")
-print(f"Download - {internetSpeed.download() / 1_000_000:.2f} Mbps")
-print(f"Upload - {internetSpeed.upload() / 1_000_000:.2f} Mbps")
+    print("\nMeasuring Internet Speed\n")
+    print(f"Download - {download:.2f} Mbps")
+    print(f"Upload - {upload:.2f} Mbps")
 
 
-# fetching best server specified:
-internetSpeed.get_servers([])
-best_server = internetSpeed.get_best_server()
-print(f"\n\nUsing server: {best_server['host']}")
-print(f"Server location: {best_server['country']}")
-print(f"Download - {internetSpeed.download() / 1_000_000:.2f} Mbps")
-print(f"Upload - {internetSpeed.upload() / 1_000_000:.2f} Mbps")
+    # fetching best server specified:
+    internetSpeed.get_servers([])
+    best_server = internetSpeed.get_best_server()
+    host_server = best_server['host']
+    country_server = best_server['country']
+    
+    print(f"\n\nUsing server: {host_server}")
+    print(f"Server location: {country_server}")
+    print(f"Download - {download:.2f} Mbps")
+    print(f"Upload - {upload:.2f} Mbps")
+
+except Exception as e:
+    print(f"Problem is: {e}")
+
