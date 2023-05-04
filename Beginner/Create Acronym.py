1d957d61c5332880cba204adfd35d2d182d58363
Ace <shohruh.nx@outlook.de>
2023-05-04 22:20:28 +0400

    Update Create Acronym.py

--- Beginner/Create Acronym.py
+++ Beginner/Create Acronym.py
@@ -1,22 +1,16 @@
 
-class CreateAcronym:
-    def __init__(self, text: str) -> str:
-        self.__text = text
-    
-    def create_acronym(self):
-        words = self.__text.split()
-        acronym = ""
-        for word in words:
-            acronym += word[0].upper()
-        return acronym
+def create_acronym(text: str) -> str:
+    words = text.split()
+    acronym = ""
+    for word in words:
+        acronym += word[0].upper()
+    return acronym
 
-text = input("Enter here: ")
-
-acronym = CreateAcronym(text)
 
 try:
-    result = acronym.create_acronym()
-    print(f"\n'{text}' acronym: {result}")
+    text = input("Enter here: ")
+    acronym = create_acronym(text)
+    print(f"\n'{text}' acronym: {acronym}")
 
 except Exception as e:
     print(f"Problem is: {e}")
