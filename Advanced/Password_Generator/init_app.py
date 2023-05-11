33ec73822874636d5c3e66e3b6589b1184dcd9d7
Ace <shohruh.nx@outlook.de>
2023-05-11 08:37:53 +0400

    Update init_app.py

--- Advanced/Password_Generator/init_app.py
+++ Advanced/Password_Generator/init_app.py
@@ -1,11 +1,26 @@
 
-from password_generator import generate_password
+from password_generator import generate_password, save_to_excel, save_to_text_file
 
 
 try:
-    passlen = int(input("\nLength: "))
+    passlen = int(input("\nPasslen: "))
+
+    print("\nOptions:")
+    print("1. All mixed")
+    print("2. Letters only")
+    print("3. Digits only")
+    print("4. Special Characters only")
+    print("5. Letters + Digits")
+    print("6. Letters + Special Characters")
+    print("7. Digits + Special Characters")
+
+    characters = input("\nChoice: ")
+    password = generate_password(passlen, characters)
+    print(f"\n\u001b[38;5;3;3mPassword: \u001b[38;5;27m{password}\u001b[0m")
+
+    save_to_excel(password)
+    save_to_text_file(password)
+    
 except Exception as e:
     print(f"\n\u001b[3m** Problem is: \u001b[38;5;196m{e}\u001b[0m")
 
-password = generate_password(passlen)
-print(f"\n\u001b[38;5;3;3mPassword: \u001b[38;5;27m{password}\u001b[0m")
