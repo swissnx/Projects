65ae648d260789eac496c73eaa42a750d453645f
Shokhrukh <shohruh.nx@outlook.de>
2023-06-04 19:40:43 +0400

    Delete AceCrypt/Password_Generator directory

--- AceCrypt/Password_Generator/PassGen.py
+++
@@ -1,9 +0,0 @@
-
-from password_generator import PasswordGenerator
-
-try:
-    passgen = PasswordGenerator()
-    passgen.run()
-
-except Exception as e:
-    print(f"\n\u001b[3m** An error occurred: \u001b[38;5;200m{e}\u001b[0m")
