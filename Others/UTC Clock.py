4dc687edaa6e9ac17a7ae8831616d3990233575b
Shokhrukh <shohruh.nx@outlook.de>
2023-06-16 02:33:36 +0400

    Update UTC Clock.py

--- Others/UTC Clock.py
+++ Others/UTC Clock.py
@@ -28,15 +28,3 @@ class TimeDisplay:
 if __name__ == "__main__":
     time_display = TimeDisplay()
     time_display.run()
-
-
-
-# ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ SHORTENED FOR BETTER UNDERSTANDING ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ #
-# from datetime import datetime
-# import time
-time_format = "%H:%M:%S"
-
-while True:
-    utc = datetime.utcnow()
-    print(f"\rTime: {utc.strftime(time_format)}", end="")
-    time.sleep(1)
