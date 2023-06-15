516cfefa56629fdb737fc2a2d8e20898f18a75f6
Shokhrukh <shohruh.nx@outlook.de>
2023-06-16 02:33:11 +0400

    Update Palindrome.py

--- Others/Palindrome.py
+++ Others/Palindrome.py
@@ -48,27 +48,3 @@ if __name__ == "__main__":
 
 # ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ DOCUMENTATION ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ #
 # A palindrome is a word or a phrase that is the same whether you read it backwards or forwards, for example the word ' refer'.
-
-"""   It's a word which look the same when read forward and backward. For example, "kayak" is a palindrome, while "loyal" is not.
-Your task is to write a program which:
-   • asks the user for some text;
-   • checks whether the entered text is a palindrome, and prints result.
-
-Note:
-   • assume that an empty string isn't a palindrome;
-   • treat upper- and lower-case letters as equal;
-   • spaces are not taken into account during the check - treat them as non-existent;
-   • there are more than a few correct solutions - try to find more than one.
-
-Test:
-   Ten animals I slam in a net
-   Eleven animals I slam in a net                  """
-
-text = input("Enter text: ")
-
-text = text.replace(' ','')    # remove all spaces...
-
-if len(text) > 1 and text.upper() == text[::-1].upper():   # ... and check if the word is equal to reversed itself
-	print("It's a palindrome")
-else:
-	print("It's not a palindrome")
