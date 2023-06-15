d535e671116e6b8705feb1749de55027159431f4
Shokhrukh <shohruh.nx@outlook.de>
2023-06-16 02:27:59 +0400

    Update Collatz Conjecture.py

--- Math/Collatz Conjecture.py
+++ Math/Collatz Conjecture.py
@@ -36,24 +36,6 @@ if __name__ == "__main__":
 
 
 # ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ DOCUMENTATION ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ #
-# This code is a Python script that calculates the number of steps required to reach 1 in a mathematical process called the Collatz Conjecture.
-c0 = int(input("Enter c0: "))     # The script prompts the user to enter an integer value for "c0", and stores it in the variable "c0".
-
-if c0 > 1:                        # Checks if the value of "c0" is greater than 1. If the value of "c0" is greater than 1, the script performs the following actions:
-    steps = 0                     # Initializes a variable "steps" to 0, which will be used to store the number of steps required to reach 1.
-    while c0 != 1:                # Uses a "while" loop to iterate through the Collatz Conjecture process. The loop continues to run until the value of "c0" becomes 1.
-        if c0 % 2 != 0:           # Inside the while loop, there is an "if-else" statement that checks if the value of "c0" is odd or even.
-            cnew = c0 * 3 + 1     # If the value of "c0" is odd, it calculates the next value "cnew" of the Collatz Conjecture process by the formula 3 * c0 + 1.
-        else:
-            cnew = c0 // 2        # If the value of "c0" is even, it calculates the next value "cnew" of the Collatz Conjecture process by the formula c0 // 2.
-        print(c0)                 # Prints the current value of "c0" using the "print" function.
-        c0 = cnew                 # Assigns the value of "cnew" to "c0" in order to use it in the next iteration of the loop.
-        steps += 1                # Increments the value of "steps" by 1, using the line "steps += 1"
-    print(f"\nSteps = {steps}\n")   # The loop continues to run til the value of "c0" becomes 1, at this point the script prints the number of steps taken to reach 1.
-else:
-    print("Bad c0 value")         # If the value of "c0" is not greater than 1, the script enters the "else" block.
-
-
 # The Collatz Conjecture is an unsolved mathematical problem. It states that for any positive integer n, if you take the following process:
 # • if n is even, divide it by 2
 # • if n is odd, multiply it by 3 and add 1
