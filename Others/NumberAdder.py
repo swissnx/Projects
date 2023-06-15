0bbb2cb48e3417a45bc1ee018f7847adab70cf63
Shokhrukh <shohruh.nx@outlook.de>
2023-06-16 02:32:57 +0400

    Update NumberAdder.py

--- Others/NumberAdder.py
+++ Others/NumberAdder.py
@@ -19,17 +19,3 @@ class NumberAdder:
 line = input("Enter Numbers (separated with spaces): ")
 number_adder = NumberAdder(line)
 number_adder.result()
-
-
-
-# ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ DOCUMENTATION ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ #
-
-line = input("Enter a line of numbers - separate them with spaces: ")   # ask the user to enter a line filled with any number of numbers (the numbers can be floats)
-strings = line.split()                                                  # split the line receiving a list of substrings;
-total = 0                                                               # initiate the total sum to zero;
-try:                            # as the string-float conversion may raise an exception, it's best to continue with the protection of the try-except block;
-    for substr in strings:
-        total += float(substr)       # ...and try to convert all its elements into float numbers; if it works, increase the sum;
-    print("The total is:", total)
-except:
-    print(substr, "is not a number.")
