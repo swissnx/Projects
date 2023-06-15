4a392476b098a058ac0bd5dd86f8eed6fe7e6150
Shokhrukh <shohruh.nx@outlook.de>
2023-06-16 02:33:20 +0400

    Update Pangram.py

--- Others/Pangram.py
+++ Others/Pangram.py
@@ -38,20 +38,3 @@ if __name__ == "__main__":
 
 # ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ FUNCTION ✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶ #
 # A pangram is a sentence that contains every letter of the alphabet at least once.
-
-def pangram(sentence):
-  char = [chr(i) for i in range(97, 96+27)]
-  print(char)
-  c = 0
-
-  for i in range(len(sentence)):
-    if sentence[i] in char and sentence[i] not in sentence[:i]:
-      c += 1
-
-  if c == 26:
-    return True
-  return False
-
-
-user = input("> ")
-print(pangram(user))
